3e530b116e779299a079048ac2752c2a1188e5f1
ashfaaq <muzammil.ashfaaq@gmail.com>
2023-11-26 03:33:12 +0530

    preprocessing done.

---
+++ main.py
@@ -0,0 +1,25 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('formated_data.csv')
+df = df.sort_values(by='date')
+
+# Initialize the Dash app
+app = dash.Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div(children=[
+    html.H1("Sales Data Visualizer"),
+
+    dcc.Graph(
+        id='line-chart',
+        figure=px.line(df, x='date', y='sales', color='region', title='Sales Over Time')
+    )
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
