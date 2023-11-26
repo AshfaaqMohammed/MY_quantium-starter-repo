2d7829ab7e4a7019878c9d404337f408f4fce039
ashfaaq <muzammil.ashfaaq@gmail.com>
2023-11-26 15:13:29 +0530

    visualization done

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
@@ -10,13 +10,38 @@ app = Dash(__name__)
 
 
 app.layout = html.Div(children=[
-    html.H1("Sales Data Visualizer"),
+    html.H1("Sales Data Visualizer", style={'text-align': 'center'}),
+    dcc.RadioItems(
+            id='region-radio',
+            options=[
+                {'label': 'All Regions', 'value': 'All'},
+                {'label': 'North', 'value': 'north'},
+                {'label': 'South', 'value': 'south'},
+                {'label': 'East', 'value': 'east'},
+                {'label': 'West', 'value': 'west'},
 
+            ],
+            value='All',
+            labelStyle={'display': 'inline-block', 'margin-right': '20px'},  # Display options horizontally with spacing
+            style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'center', 'border': '1px solid #ddd', 'padding': '10px', 'border-radius': '5px'},  # Center the group and add a box around it
+        ),
     dcc.Graph(
         id='line-chart',
-        figure=px.line(df, x='date', y='sales', title='Sales Over Time')
+        figure=px.line(df, x='date', y='sales',color='region', title='Sales Over Time')
     )
 ])
+
+
+@app.callback(Output('line-chart', 'figure'), [Input('region-radio', 'value')])
+def update_graph(selected_region):
+    if selected_region == 'All':
+        filtered_df = df
+    else:
+        filtered_df = df[df['region'] == selected_region]
+
+    fig = px.line(filtered_df, x='date', y='sales', title='Sales Over Time')
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
